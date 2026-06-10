0a93fc37b80e87c3dfe433c735a9dd727755bcf4
gert-vercleyen <gert.vercleyen@protonmail.com>
2026-06-10 14:55:35 +0200

    +yet another test file :)

---
+++ notebooks/general_testing.jl
@@ -0,0 +1,15 @@
+using Revise, Pkg, Oscar
+Pkg.activate("/home/gert/Projects/FusionRings.jl/")
+using FusionRings
+
+
+
+function multtabcode( mt::Array{Int,3}, mult::Int )::ZZRingElem
+    m = mult + 1
+    digits_base_m = [ mt[i] for i in eachindex(mt) ]
+    digits_string = join(string.(digits_base_m))
+
+    ZZ( parse( BigInt, digits_string, base = m ) )
+end
+
+multtabcode( frl[30] |> multiplication_table, 1 )

9ca53b79843467e9736e4c8639ba0cdaa339eb64
gert-vercleyen <gert.vercleyen@protonmail.com>
2026-06-24 10:43:18 +0200

    fixed HI nonabelian group error

--- test/creation_tests.jl
+++ test/creation_tests.jl
@@ -801,13 +801,23 @@
       2 1
     ]
 
-    z3_tab = [
+    #= z3_tab = [
       1 2 3
       2 3 1
       3 1 2
     ]
+    =#
 
-    nonsymmetric_group_tab = z3_tab
+    D3_tab = [
+      1 2 3 4 5 6 
+      2 3 1 6 4 5
+      3 1 2 5 6 4
+      4 5 6 1 2 3
+      5 6 4 3 1 2
+      6 4 5 2 3 1
+    ]
+
+    nonsymmetric_group_tab = D3_tab
     bad_tab = [
       1 1
       2 2
